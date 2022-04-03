770294b8d964b481ca86c887a9b50d0d0d3234ce
Aldo pereira <aldopereira.gyn@gmail.com>
2022-04-03 20:05:04 -0300

    Página de dashboard

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,5 +1,10 @@
 import { useLocation } from "react-router-dom";
 import Message from "../layouts/Message";
+import styles from './Projects.module.css'
+import { Link } from "react-router-dom"
+import Container from "../layouts/Container";
+import LinkButton from "../layouts/LinkButton";
+
 
 function Projects() {
 
@@ -9,10 +14,20 @@ function Projects() {
         message = location.state.message
     }
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+
+        <div className={styles.project_container}>
+            <div className={styles.title_container} >
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newprojects" text="Criar Projeto" />
+
+            </div>
             {message && <Message type="success" msg={message} />}
+            <Container custonClass="start" >
+                <p>Projetos...</p>
+
+            </Container>
         </div>
+
     )
 
 }
